6a1d8589213a096f82374380de4a38eb6593db0a
JedTiNon <jedtinon_non@hotmail.com>
2021-02-21 19:00:32 +0700

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php  
- //$connect = mysqli_connect("127.0.0.1","root","","smart_q"); 
- $connect = mysqli_connect("192.168.200.245","bantak","11238","bantak_hosxp");
- mysqli_set_charset($connect, "utf8");
-
- $connect2 = mysqli_connect("127.0.0.1","root","","smart_q"); 
- mysqli_set_charset($connect2, "utf8");
- 
- ?>
